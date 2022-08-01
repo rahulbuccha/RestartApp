9756f27e2f43f9ef4226ef4fda3564f40c21b0c3
Rahul Shaw <rahulbucha69@gmail.com>
2022-08-01 13:08:46 +0530

    Initial Commit

---
+++ RestartApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestartApp-SwiftUI
+//
+//  Created by Admin on 01/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp-SwiftUI/RestartApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp_SwiftUIApp.swift
+//  RestartApp-SwiftUI
+//
+//  Created by Admin on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
